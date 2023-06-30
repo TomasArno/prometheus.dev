3cc1ced25c08aed7b2bc1cb258106904fda62790
Matias Arno <amatiasarno@gmail.com>
2023-06-30 01:05:16 -0300

    Actualización de tipografía Poppins. Navbar nuevo.

--- client/pages/welcome/index.ts
+++ client/pages/welcome/index.ts
@@ -311,27 +311,40 @@ customElements.define(
 
     render() {
       this.shadow.innerHTML = `
-		
-		<div class="main">
-			<div class="complete-draw">
-				<div class="left-corner">
-					<div class="draw top"></div>
-					<div class="draw left"></div>
-				</div>
-				<div class="main_content">
-					<div class="welcome">
-						<h1 class="welcome_title"><span class="animated_span" style="--start-color:#007CF0; --end-color:#00DFD8; --content: 'Plan';">Plan</span> <span class="h1_arrow">></span> <span class="animated_span" style="--start-color:#7928CA; --end-color:#FF0080; --content: 'Develop'; --animation:a2;">Develop</span> <span class="h1_arrow">></span> <span class="animated_span" style="--start-color:#FF4D4D; animation-name:a3; --end-color:#F9CB28; --content: 'Deploy'; --animation: a3">Deploy</span></h1>
-						<nav class="welcome_nav"><ul><li><a href="welcome">Welcome</a><span class="nav_li_separator">|</span></li><li><a href="about-us">About Us</a><span class="nav_li_separator">|</span></li><li><a href="projects">Projects</a><span class="nav_li_separator">|</span></li><li><a href="contact">Contact</a></li></ul></nav>
-					</div>
-				</div>
-				<div class="right-corner">
-					<div class="draw rigth"></div>
-					<div class="draw bottom"></div>
-				</div>
-			</div>
-		</div>
-		
-		`;
+
+      		<div class="main">
+      			<div class="complete-draw">
+
+      				<div class="left-corner">
+      					<div class="draw top"></div>
+      					<div class="draw left"></div>
+      				</div>
+
+      				<div class="main_content">
+      					<div class="welcome">
+							<nav class="welcome_nav">
+								<ul>
+									<li><a href="#">Welcome</a></li>
+									<li class="nav_li_separator">-</li>
+									<li><a href="#">About Us</a></li>
+									<li class="nav_li_separator">-</li>
+									<li><a href="#">Projects</a></li>
+									<li class="nav_li_separator">-</li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</nav>
+      						<h1 class="welcome_title">Plan <span class="h1_arrow">►</span> Develop <span class="h1_arrow">►</span> Deploy</h1>
+      					</div>
+      				</div>
+
+      				<div class="right-corner">
+      					<div class="draw rigth"></div>
+      					<div class="draw bottom"></div>
+      				</div>
+      			</div>
+      		</div>
+
+      		`;
 
       this.addStyles();
     }
